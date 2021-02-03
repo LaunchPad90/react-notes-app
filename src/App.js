0eb9838ae3785ee8c6df9cdc5d3790c2343210c3
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2021-02-03 10:59:07 -0700

    text data saved into local storage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import { useState } from 'react'
+
 import './App.css';
 
+
 function App() {
+  
+  const [noteText, setNoteText] = useState('')
+
+  const handleNoteTextChange = (e) => {
+    setNoteText(e.target.value);
+  }
+
+  const handleSaveNote = () => {
+    localStorage.setItem('newNote', noteText);
+    console.log(localStorage)
+  }
+
+  const handleDeleteNote = () => {
+    localStorage.clear();
+    console.log(localStorage)
+  }
+
   return (
     <div className="App">
-      <h1>Home</h1>
+      <div className="note-container">
+        <div className="top-bar">
+          <button onClick={handleDeleteNote} className="delete-button">X</button>
+        </div>
+        <textarea value={noteText} onChange={handleNoteTextChange} className="text-container"></textarea>
+        <button onClick={handleSaveNote}>Save Note</button>
+      </div>
+      <div>{localStorage.getItem('newNote')}</div>
     </div>
   );
 }
